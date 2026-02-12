1e173e58973ebffac248f697d66d99395030bb2a
Willi Kappler <willi.kappler@uni-tuebingen.de>
2026-02-12 09:48:44 +0100

    Refactor compile flags.

--- xmake.lua
+++ xmake.lua
@@ -22,16 +22,14 @@ set_project("selecppion")
 -- xmake -a -r
 
 -- For all compilers:
--- set_warnings("error", "everything", "extra", "pedantic", "all")
+set_warnings("all", "extra", "pedantic", "error")
 
 -- For GCC and Clang only:
 if is_kind("gcc", "clang") then
     -- For GCC only:
     if is_kind("gcc") then
-        set_warnings("everything", "all", "extra", "pedantic", "error")
         add_cxxflags("-Walloca", "-Wcast-align=strict", "-Wimplicit-fallthrough=5")
     elseif is_kind("clang") then
-        set_warnings("all", "extra", "pedantic", "error")
         -- Clang prefers the boolean flag without the numeric level
         add_cxxflags("-Wimplicit-fallthrough")
     end
@@ -44,7 +42,6 @@ end
 
 -- For MSVC only:
 if is_kind("cl") then
-    set_warnings("everything", "all", "extra", "pedantic", "error")
     -- add_cxxflags("/Wall")
     add_cxxflags("/W4") -- Level 4 is the standard "Strict" for MSVC
     add_cxxflags("/w14242") -- 'identifier': conversion from 'type1' to 'type1', possible loss of data
